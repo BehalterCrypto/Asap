e06f495e8cfe8aab8b57d46e20f12ea4cc08e55d
Behalter Crypto <121450230+BehalterCrypto@users.noreply.github.com>
2026-06-08 00:32:39 +0700

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import Dashboard from "./pages/Dashboard";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <div>
+      <Dashboard />
+      <Navbar />
+    </div>
+  );
+}
